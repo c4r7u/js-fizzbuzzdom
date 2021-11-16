bd4d74b0eaedbcb17af068a7f37ce246371c17cf
c4r7u <nicolo.carluccio3@gmail.com>
2021-11-16 16:15:29 +0100

    js milestone 1

--- milestone 1/js/script.js
+++ milestone 1/js/script.js
@@ -1,5 +1,18 @@
-const ul = document.querySelector('ul.box')
+/* Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina “La password assegnata è: ” nomecognomecolorepreferito21  */
 
-for (let i = 1; i <= 100; i++) {
-    
-}
+// i ask the user for his name "chiedo all'utente il suo nome"
+const name = prompt("Dimmi il tuo nome");
+
+// i ask the user for his surname "chiedo all'utente il suo cognome"
+const surname = prompt("Dimmi il tuo cognome");
+
+// i ask for his favorite color "chiedo il suo colore preferito"
+const favoriteColor = prompt("Dimmi il tuo colore preferito");
+
+// i write on the page "la password assegnata é: " namesurnamefavoritecolor21
+let password = `${name}${surname}${favoriteColor}`;
+document.getElementById("generatore-password")
+.innerHTML= `La password assegnata é: ${password}21`;
